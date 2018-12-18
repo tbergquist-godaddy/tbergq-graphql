bb46b69891cb095208ad104efc61218861c7463a
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-18 13:26:15 +0100

    fix typo

--- src/RootMutation.js
+++ src/RootMutation.js
@@ -4,14 +4,14 @@ import { GraphQLObjectType } from 'graphql';
 
 import Login from './mutations/Login';
 import tvHelperLogin from './tvhelper/mutations/Login';
-import toggeFavorite from './tvhelper/mutations/ToggleFavorite';
+import toggleFavorite from './tvhelper/mutations/ToggleFavorite';
 
 export default new GraphQLObjectType({
   name: 'RootMutation',
   description: 'Root Mutation.',
   fields: {
     login: Login,
-    toggeFavorite,
+    toggleFavorite,
     tvHelperLogin,
   },
 });
