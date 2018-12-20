f793914fb9540a6112080a1522b7d217e8e792b5
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-20 16:24:07 +0100

    Add test for isFavorite resolver

---
+++ src/tvhelper/types/__tests__/TvShow.test.js
@@ -0,0 +1,48 @@
+// @flow
+
+import { toGlobalId } from 'graphql-relay';
+
+import TvShow from '../TvShow';
+
+const fields = TvShow.getFields();
+
+const { resolve } = fields.isFavorite;
+
+const ancestor = { id: 1 };
+const args = null;
+
+it('returns null when not logged in', async () => {
+  expect(await resolve(ancestor, args, { user: null, dataLoader: {} })).toBe(
+    null,
+  );
+});
+
+it('returns true when show is favorite', async () => {
+  const user = {
+    id: toGlobalId('user', '1'),
+  };
+  const dataLoader = {
+    tvhelper: {
+      favorite: {
+        load: async () => Promise.resolve({ userId: 1, serieId: 1 }),
+      },
+    },
+  };
+  const context = { user, dataLoader };
+  expect(await resolve(ancestor, args, context)).toBe(true);
+});
+
+it('returns false when show is not favorite', async () => {
+  const user = {
+    id: toGlobalId('user', '1'),
+  };
+  const dataLoader = {
+    tvhelper: {
+      favorite: {
+        load: async () => Promise.resolve(null),
+      },
+    },
+  };
+  const context = { user, dataLoader };
+  expect(await resolve(ancestor, args, context)).toBe(false);
+});
